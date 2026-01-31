e42732655cbec4855a03204c5ef92eaf739f240d
novalgine <153831098+novalgine@users.noreply.github.com>
2026-01-31 18:30:06 +0300

    fix(build): add @react-pdf/renderer to serverComponentsExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
+  },
 };
 
 export default withPWA({
